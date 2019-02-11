943ed78c8d40ab7c4bc35689b2f738a5caca20b8
Martin Hornacek <hornacek.martin@gmail.com>
2019-02-11 19:59:04 +0100

    fixed the issue with polyfills and added the CRM solution

--- Deploy/AngularHelloWorld/src/app/app.module.ts
+++ Deploy/AngularHelloWorld/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { APP_BASE_HREF } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

0f81c50d3f6afd29c9d469c9913014c65c7e21c5
Martin Hornacek <hornacek.martin@gmail.com>
2019-02-12 21:39:58 +0100

    added draft of crm.service.ts

--- Deploy/AngularHelloWorld/src/app/app.component.ts
+++ Deploy/AngularHelloWorld/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CrmService, Entity } from './crm.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularHelloWorld';
+  errorMessage: string = null;
+  accounts: Entity[] = [];
+
+  constructor(private crmService: CrmService) { }
+
+  ngOnInit() {
+    this.crmService
+      .search("accounts", "name", null)
+      .subscribe(entities => this.accounts = entities, error => this.errorMessage = <any>error);
+  }
 }
